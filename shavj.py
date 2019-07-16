a8c76990287c4aeae2c8417cfdbe131a6b7fc1f7
shan1015 <52270808+shan1015@users.noreply.github.com>
2019-07-16 14:10:52 +0530

    Create shavj.py

---
+++ shavj.py
@@ -0,0 +1,13 @@
+# of a number n without using loop. 
+def multiple(m, n): 
+ 
+    # inserts all elements from n to  
+    # (m * n)+1 incremented by n. 
+    a = range(n, (m * n)+1, n) 
+ 
+    print(*a) 
+ 
+# driver code  
+m = 4
+n = 3
+multiple(m, n)
